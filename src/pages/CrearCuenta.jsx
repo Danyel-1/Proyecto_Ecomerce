733f8512630b13182f107bf5d3d0427a5cfa9df8
Jose Daniel <danngto99@gmail.com>
2026-06-05 01:32:03 -0600

    Crear cuenta rehecho

--- src/pages/CrearCuenta.jsx
+++ src/pages/CrearCuenta.jsx
@@ -1,10 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CrearCuenta = () => {
+  const [botonSubmit, setBotonSubmit] = useState(false);
+  const [mensajeError, setMensajeError] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [errorNombre, setErrorNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  const validarDatos=()=>{
+    const nomRegex = /^[a-zA-Z\s]{8}/;
+    const emailRegex = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i;
+
+    if ((nombre.length >= 0) && (nomRegex.test(nombre))){
+      setBotonSubmit(true);
+      setErrorNombre('correcto')
+    }else{
+      setErrorNombre("El nombre debe tener minimo 8 caracteres");
+      setBotonSubmit(false);
+    }
+  
+    if ((email.length >= 0) && (emailRegex.test(email))){
+      setErrorEmail("Correcto");
+      setBotonSubmit(true);
+    }else{
+      setErrorEmail("Escriba su Email en un formato valido");
+      setBotonSubmit(false);
+    }
+  }
+
+  async function mandarDatos(e) {
+    e.preventDefault();
+
+    try {
+      const responce = await fetch("https://api.escuelajs.co/api/v1/users/", {
+        method: "POST",
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({
+          name: nombre,
+          email: email,
+          password: password,
+          avatar: "https://picsum.photos/800"
+        })
+      });
+
+      if (!responce.ok) {
+        throw new Error(`Responce status: ${responce.status}`)
+      }
+
+    } catch (error) {
+      setMensajeError(`Response estatus: ${error.message}`)
+    }
+
+    console.log("Correcto");
+      
+  }
+
   return (
-    <div>
-      <h1 className='crear-cuenta'>Crear Cuenta</h1>
-    </div>
+    <section className='formulario-ingreso'>
+      <h1>Ingresa un correo electronico y una contrasena</h1>
+
+      <form onChange={validarDatos} onSubmit={mandarDatos}>
+        <label htmlFor="nombre">Nombre</label>
+        <br />
+        <input 
+        id='nombre' 
+        type="text" 
+        name='nombre' 
+        placeholder='Nombre' 
+        onChange={(e) => setNombre(e.target.value)}
+        required/>
+        {errorNombre}
+        <br />
+
+        <label htmlFor="email">Correo Electronico</label>
+        <br />
+        <input 
+        id='email' 
+        type="email" 
+        name='email' 
+        placeholder='correo@correo.com'
+        onChange={(e) => setEmail(e.target.value)}
+        required/>
+        {errorEmail}
+        <br />
+
+        <label htmlFor="password">Contrasena</label>
+        <br />
+        <input 
+        id='password' 
+        type="password" 
+        name='password' 
+        placeholder='******'
+        onChange={(e) => setPassword(e.target.value)}
+        required/>
+        {errorPassword}
+
+        <div className='botones'>
+          <button  type='submit' disabled={!botonSubmit}>Continuar</button>
+        </div>
+      </form>
+
+    </section>
   )
 }
 
--- src/pages/IngresarCuenta.jsx
+++ src/pages/IngresarCuenta.jsx
@@ -1,98 +1,12 @@
 import React, { useState } from 'react'
 
 const IngresarCuenta = () => {
-  const [inputs, setInputs] = useState({});
-  const [nombreValidado, setNombreValidado] = useState(false);
-  const [emailValidado, setEmailValidado] = useState(false);
-  const [mensajeNombre, setMendajeNombre] = useState("");
-  const [mensajeEmail, setMendajeEmail] = useState("");
 
-  
-  const handleChange = (e) =>{
-    const name = e.target.name,
-    value = e.target.value;  
-    
-    if(name === "nombre"){
-      const nomRegex = /^[a-zA-Z\s]{8}/;
-      
-      if ((value.length >= 0) && (nomRegex.test(value))) {
-        setNombreValidado(true);
-        setMendajeNombre("Espacio completado ✅");
-
-      }else{
-        setNombreValidado(false);
-        setMendajeNombre("El espacio en 'nombre' es necesario");
-      }
-    }
-
-    setInputs(values => ({...values, [name] : value}))
-  }
-
-    const mandarDatos = () => {
-
-        fetch("https://api.escuelajs.co/api/v1/users/", {
-          method: "POST",
-          headers: {'Content-Type' : 'application/json'},
-          body: JSON.stringify({
-            name: inputs.nombre,
-            email: inputs.email,
-            password: inputs.password,
-            avatar: "https://picsum.photos/800"
-          })
-        })
-        .then(response =>{
-          if (!response.ok) throw new Error(`Response status: ${response.status}`);
-          return response.json();
-        })
-        .catch(error => console.error(error)
-        );       
-    }
 
   return (
-    <section className='formulario-ingreso'>
-      <h1>Ingresa un correo electronico y una contrasena</h1>
-
-      <form >
-        <label htmlFor="nombre">Nombre</label>
-        <br />
-        <input 
-        id='nombre' 
-        type="text" 
-        name='nombre' 
-        placeholder='Nombre' 
-        value={inputs.nombre}
-        onChange={handleChange}
-        required/>
-        {mensajeNombre}
-        <br />
-        <label htmlFor="email">Correo Electronico</label>
-        <br />
-        <input 
-        id='email' 
-        type="email" 
-        name='email' 
-        placeholder='correo@correo.com' 
-        value={inputs.email}
-        onChange={handleChange}
-        required/>
-        <br />
-
-        <label htmlFor="password">Contrasena</label>
-        <br />
-        <input 
-        id='password' 
-        type="password" 
-        name='password' 
-        placeholder='******'
-        value={inputs.password}
-        onChange={handleChange}
-         required/>
-      </form>
-
-    <div className='botones'>
-        <button onClick={mandarDatos}>Continuar</button>
+    <div>
+      <h1 className='crear-cuenta'>Loguea tu Cuenta</h1>
     </div>
-    </section>
   )
 }
 
